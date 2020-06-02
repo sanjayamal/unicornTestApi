9c613013208e5b5428b6704e66752a11c9b448cc
rajithasanjayamal0918@gmail.com <rajithasanjayamal0918@gmail.com>
2020-06-02 12:30:10 +0530

    webApi initiate

---
+++ Repository/IRepositorycs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApi.Repository
+{
+    interface IRepositorycs<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+
+        bool Delete(int id);
+    }
+}
